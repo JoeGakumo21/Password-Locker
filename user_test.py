7e3f0e043f98032b532d9fe78bff4886ce6f1735
Joe Gakumo <joegakumo1@gmail.com>
2021-09-05 22:34:19 +0300

    add delete,search and credetial test

--- user_test.py
+++ user_test.py
@@ -29,56 +29,40 @@ class  UserTest(unittest.TestCase):
         self.user.save_user()
         self.assertEqual(len(User.users),1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # def test_init(self):
-    #     '''
-    #     test_init test case to test if the object is initialized properly
-    #     '''
-
-    #     self.assertEqual(self.new_user.username,"Joseph Gakumo")
-    #     self.assertEqual(self.new_user.password,"Joegakumo123456")
-    #     # self.assertEqual(self.new_user.accountname,"Facebook")
-    #     # self.assertEqual(self.new_user.fullname," Joseph Gakumo")
-    #     # self.assertEqual(self.new_user.email,"joegakumo1@gmail.com")
-
-    # #creation of tearDown here
-    # def tearDown(self):
-    #      '''
-    #     tearDown method that does clean up after each test case has run. 
-    #      '''
-    #      User.user_list=[]
+    #third test
+    def test_save_multiple_users(self):
+        '''
+        test to be tested when multiple users are saved
+        '''    
+        User.users = []
+        self.user.save_user()
+        test_user = User('test', 'test')
+        test_user.save_user()
+        self.assertEqual(len(User.users), 2)
+    def test_delete_user(self):
+        '''
+        test to delete a user
+        '''
+        # User.users = []
+        self.user.save_user()
+        test_user = User('test', 'test')
+        test_user.save_user()
+        self.user.delete_user(test_user.username)
+        self.assertEqual(len(User.users), 1)
     
+    def tearDown(self):
+        User.users = []
 
-    # #second test
-    # def test_save_user(self):
-    #     '''
-    #     test_save_user_details test case to test if the user object is saved into
-    #      the object list
-    #     '''
-    #     self.new_user.save_user() # saving the new contact
-    #     self.assertEqual(len(User.user_list),1)
 
-    # #third test 
-    # def test_save_multiple_users(self):
-    #     '''
-    #     method to save multiple account in the same user locker password
-    #     '''     
-    #     self.new_user.save_user()
-    #     test_user=User("Joseph Gakumo", "Joegakumo1234455")
-    #     test_user.save_user()
-    #     self.assertEqual(len(User.user_list),2)
+# credentials test
+#first test
+
+class CredentialTest(unittest.TestCase):
+    def setUp(self):
+        '''
+          Set up method to run before each test cases.
+        '''
+       
 
 if __name__ == '__main__':
     unittest.main()

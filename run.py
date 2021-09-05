f6515761175e3d343e60c777c6a61d71d6dc9d46
Joe Gakumo <joegakumo1@gmail.com>
2021-09-05 03:37:44 +0300

    add class user

--- run.py
+++ run.py
@@ -3,4 +3,25 @@ import random
 from user import User
 from credential import Credentians
 # user class here
-        
+
+
+class User:
+    '''
+    class that generate new instance of users
+    '''
+    user_list=list() #empty userdetails list
+
+   
+    
+    def __init__(self, username, password):
+        '''
+        __init__ method that helps us define properties for our object
+        '''
+        self.username=username
+        self.password=password
+
+    def full_account_details(self):
+        '''
+        method to display all details of user
+        '''
+        print(f"Your username is:{self.username}   and your password is:{self.password}")

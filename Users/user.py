0cd2b44501c21d15f7c452e41c1036eb89793133
Joe Gakumo <joegakumo1@gmail.com>
2021-09-05 20:19:43 +0300

    path change for users and add more functions

---
+++ Users/user.py
@@ -0,0 +1,60 @@
+class User:
+    '''
+    A user class
+    '''
+
+    users= []
+    def __init__(self,username, password) :
+        """
+        Args:
+        Username(string)
+        password(string)
+        """
+        self.username=username
+        self.password=password
+
+    #method to save user details
+    def save_user(self):
+        '''        
+        saving Users
+        '''     
+        User.users.append(self)
+
+        #method to display username in string
+    def __repr__(self) -> str:
+        return f"{self.username}"  
+
+
+    @classmethod
+    def show_user(cls):
+        return cls.users      
+
+    def find_by_username(cls,username):
+        '''
+        method to find the user using username
+        '''    
+        for user in cls.users:
+            if user.username==username:
+                return user
+    def find_user(cls):
+        '''
+        method to find user in the list
+        '''            
+        return cls.users
+
+    def delete_user(cls,username):
+        '''
+        method to delete an existing user
+        '''
+        for user in cls.users:
+            if username==username:
+                cls.users.remove(user)
+                return True
+            return False    
+
+    def del_user(self):
+        '''
+        method to delete a single user
+        '''
+        User.users.remove(self)
+        return True

fb5d902e5399dd5227efa58b2e0b287d99410aa1
Joe Gakumo <joegakumo1@gmail.com>
2021-09-05 03:13:21 +0300

    add class user

---
+++ user.py
@@ -0,0 +1,22 @@
+class User:
+    '''
+    class that generate new instance of users
+    '''
+    user_list=[] #empty userdetails list
+
+    
+    def __init__(self, username, password):
+        '''
+        __init__ method that helps us define properties for our object
+        '''
+        self.username=username
+        self.password=password
+
+    # def  full_details_user(self):
+    #     obj=Credentians()
+    #     data= obj.Credentians()
+    def save_user(self):
+        '''
+        save userdetails method into users password lock
+        '''
+        User.user_list.append(self)
